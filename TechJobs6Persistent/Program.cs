b051ecf1be18583573450ce4c08b435324e75af7
Elizabeth Levinson <lizarose266@gmail.com>
2024-06-21 10:07:30 -0500

    Task 1 Completed - SQL set up and connected

--- TechJobs6Persistent/Program.cs
+++ TechJobs6Persistent/Program.cs
@@ -6,6 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+var connectionString = "server=localhost;user=techjobs;password=techjobs;database=techjobs";
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
+
+builder.Services.AddDbContext<JobDbContext>(dbContextOptions => dbContextOptions.UseMySql(connectionString, serverVersion));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
